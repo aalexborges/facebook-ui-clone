d688f8db96b51f6ae8a569e0a5e55b51ede3b091
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-11-11 09:06:01 -0300

    v0.1.2
    Reaction icons are now no longer selectable.

--- src/components/PostReactionsMap/styles.ts
+++ src/components/PostReactionsMap/styles.ts
@@ -4,8 +4,13 @@ export const Container = styled.div<{
   size?: number
   move?: { left: boolean; index: number }
 }>`
+  z-index: ${({ move }) => (move?.left ? 6 - move.index : 6)};
+
   width: ${({ size }) => size || 22}px;
   height: ${({ size }) => size || 22}px;
+
+  left: ${({ move }) => (move?.left ? `${-4 * move.index}px` : 'unset')};
+  position: relative;
 
   display: flex;
   align-items: center;
@@ -14,13 +19,12 @@ export const Container = styled.div<{
   border: 4px solid ${({ theme }) => theme.surfaceBackground};
   border-radius: 50%;
 
-  z-index: ${({ move }) => (move?.left ? 6 - move.index : 6)};
-
-  left: ${({ move }) => (move?.left ? `${-4 * move.index}px` : 'unset')};
-  position: relative;
+  cursor: pointer;
+  user-select: none;
 
   & > img {
     width: calc(${({ size }) => size || 22}px - 4px);
     height: calc(${({ size }) => size || 22}px - 4px);
+    user-select: none;
   }
 `
